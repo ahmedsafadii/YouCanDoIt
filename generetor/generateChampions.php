374c7c4e0a996678d5fa84f2e18d91d43511bab5
Wolf Devil <ahmedsafadii@gmail.com>
2016-05-10 00:47:08 +0300

    Update generateChampions.php

--- generetor/generateChampions.php
+++ generetor/generateChampions.php
@@ -1,13 +1,17 @@
 <?php
-require('../connection.php');
+ require('../connection.php');
+  
+  
+ $file = file_get_contents('champion.json');
+  
+ $generate = json_decode($file,true);
 
-
-$file = file_get_contents('champion.json');
-
-$generate = json_decode($file,true);
-
-$id = 1;
-
-
-
-?>
+ foreach ($generate["data"] as $key => $val){
+	$champname = $val["id"];
+ 	$champid = $val["key"];
+	$champtitle = $con->real_escape_string($val["title"]);
+ 	$insertquery = $con->query("INSERT INTO champions (champion_id,champion_name,champion_title) VALUES ('$champid','$champname','$champtitle')");
+	var_dump("INSERT INTO champions (champion_id,champion_name,champion_title) VALUES ('$champid','$champname','$champtitle')");
+ }
+  
+  ?>
